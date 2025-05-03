d1ed9834aa10cd5362da2180d4d2f35a3cd251c5
sulimanarabi147 <sulimanalsaqqar0@gmail.com>
2025-05-03 11:13:53 +0300

    Add static files

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -127,22 +126,17 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-import os
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-
-
 
 # settings.py
 
-STATIC_URL = '/static/'
+import os
 
-# تحديد مجلد static داخل المشروع
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # هذا إذا كنت تستخدم Django 3.0 أو أعلى
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # إعدادات البريد
